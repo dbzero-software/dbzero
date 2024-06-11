ab310f72e0136ae3ba709376d4189ae9a344d533
Wojtek <wskozlowski@dbzero.io>
2024-06-11 12:48:31 +0200

    scoped types

--- python_tests/test_scoped_class.py
+++ python_tests/test_scoped_class.py
@@ -11,3 +11,8 @@ class DataClass:
 def test_get_type_info_from_scoped_class(db0_fixture):
     type_info = db0.get_type_info(DataClass)    
     assert type_info["prefix"] == "scoped-class-prefix"
+    
+    
+def test_create_scoped_class_instance(db0_fixture):
+    obj = DataClass(42)
+    assert db0.get_prefix(obj) != db0.get_current_prefix()
