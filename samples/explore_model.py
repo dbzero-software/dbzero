01c880dde03690057eacdb322fb7f0af9b6346e9
Wojtek <wskozlowski@dbzero.io>
2024-09-20 10:23:48 +0200

    script cleanup

--- samples/explore_model.py
+++ samples/explore_model.py
@@ -6,7 +6,7 @@ This script demonstrates techniques of exploring the DBZero prefixes
 with the model classes imported into the script.
 """
 
-def values(obj, attr_names):
+def values_of(obj, attr_names):
     return [getattr(obj, attr_name) for attr_name in attr_names]
     
 def __main__():
@@ -17,8 +17,9 @@ def __main__():
             print(prefix)
             for memo_class in db0.get_memo_classes(prefix):
                 attr_names = [attr.name for attr in memo_class.get_attributes()]
-                for obj in memo_class.all():
-                    print(values(obj, attr_names))                
+                print(attr_names)
+                for obj in memo_class.all():                    
+                    print(values_of(obj, attr_names))                    
     except Exception as e:
         print(e)
     
