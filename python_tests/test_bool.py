bc37e81a75540ffe3cba7e2bfbbdd6900a39bf68
Adrian Zawadzki <a.zawadzki@camino.science>
2024-11-11 23:41:50 +0100

    added boolean

---
+++ python_tests/test_bool.py
@@ -0,0 +1,12 @@
+
+import dbzero_ce as db0
+from datetime import datetime
+
+@db0.memo
+class MemoScopedClass:
+    def __init__(self, value, prefix=None):
+        db0.set_prefix(self, prefix)        
+        self.value = value   
+
+def test_assing_bool_to_memo(db0_fixture):
+    m = MemoScopedClass(True)
