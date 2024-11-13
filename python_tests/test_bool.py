7dd94f824c3c389a671c6bcb20c316d74741ee99
Adrian Zawadzki <a.zawadzki@camino.science>
2024-11-13 13:36:00 +0100

    changed test

--- python_tests/test_bool.py
+++ python_tests/test_bool.py
@@ -10,3 +10,6 @@ class MemoScopedClass:
 
 def test_assing_bool_to_memo(db0_fixture):
     m = MemoScopedClass(True)
+    assert m.value == True
+    m = MemoScopedClass(False)
+    assert m.value == False
