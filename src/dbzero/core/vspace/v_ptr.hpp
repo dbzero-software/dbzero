3ba584b85f3485c42f1925cb3c1cc49334399b62
Wojtek <wskozlowski@dbzero.io>
2024-11-13 13:53:49 +0100

    non-mutable access mode

--- src/dbzero/core/vspace/v_ptr.hpp
+++ src/dbzero/core/vspace/v_ptr.hpp
@@ -52,7 +52,7 @@ namespace db0
         Memspace *m_memspace_ptr = nullptr;
         mutable std::atomic<std::uint16_t> m_resource_flags = 0;
         // initial access flags (e.g. read / write / create)
-        mutable FlagSet<AccessOptions> m_access_mode;
+        FlagSet<AccessOptions> m_access_mode;
         
         /**
          * Memory mapped range corresponding to this object
@@ -134,7 +134,7 @@ namespace db0
          * Detach underlying resource lock (i.e. mark resource as not available in local memory)
         */
         void detach();
-
+        
         /**
          * Commit by marking the write as final.
          * The subsequent modify() will need to refresh the underlying lock
@@ -226,7 +226,7 @@ namespace db0
             }
             return *reinterpret_cast<ContainerT*>(m_mem_lock.modify());
         }
-        
+
         const ContainerT& safeRef() const
         {
             assureInitialized();
