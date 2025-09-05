e80afd652fbb899d5ce46e416420d34d92a8f707
Wojtek <wskozlowski@dbzero.io>
2025-09-05 09:24:52 +0200

    memo dataclass type as arg

--- python_tests/test_memo_dataclass.py
+++ python_tests/test_memo_dataclass.py
@@ -29,7 +29,7 @@ class MemoDataClassArgsFactory:
     event_type: str
     event_name: datetime = field(default_factory=datetime.now)
     
-        
+    
 def test_memo_dataclass(db0_fixture):
     obj_1 = MemoDataClass("Some Title", "Some Author", 2022, True)
     assert obj_1.title == "Some Title"
@@ -48,7 +48,7 @@ def test_memo_dataclass_kwargs_init(db0_fixture):
     assert db0.is_memo(obj_1)
 
 
-def test_memodata_class_default_args(db0_fixture):
+def test_memo_dataclass_default_args(db0_fixture):
     obj_1 = MemoDataClass("Some Title", "Some Author")
     assert obj_1.title == "Some Title"
     assert obj_1.author == "Some Author"
@@ -57,14 +57,22 @@ def test_memodata_class_default_args(db0_fixture):
     assert db0.is_memo(obj_1)
 
     
-def test_memodata_class_default_generated_args(db0_fixture): 
+def test_memo_dataclass_default_generated_args(db0_fixture):
     obj_1 = MemoDataClassGenArgs(event_type = "Some Event")
     assert obj_1.event_type == "Some Event" 
     assert obj_1.event_name == "a_b"
     
     
-def test_memodata_class_default_args_factory(db0_fixture):
+def test_memo_dataclass_default_args_factory(db0_fixture):
     obj_1 = MemoDataClassArgsFactory(event_type = "Some Event")
     assert obj_1.event_type == "Some Event"
     # less than 1ms diff
     assert abs((obj_1.event_name - datetime.now()).total_seconds()) < 0.001
+
+
+def test_memo_dataclass_type_passed_dynamically(db0_fixture):
+    def factory(the_type, **kwargs):
+        return the_type(**kwargs)
+    
+    obj_1 = factory(MemoDataClassGenArgs, event_type = "Some Event")
+    assert obj_1.event_type == "Some Event"
