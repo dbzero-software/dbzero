46c37b42b354fbaf45408a5f425807d0057c05c7
twejroch <sigsegvsoft@gmail.com>
2025-11-11 19:49:09 +0100

    Added check for endianness (#481)
    Co-authored-by: Tomasz Wejroch <twejroch@dbzero.io>

--- dbzero/setup.py
+++ dbzero/setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup
+import sys
+
+# Check for big-endian systems
+if sys.byteorder == 'big':
+    raise RuntimeError("dbzero does not support big-endian systems")
 
 setup(
     name='dbzero',
