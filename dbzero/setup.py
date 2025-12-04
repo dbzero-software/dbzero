6449060836a14c751c6907c749f5603d9be5c901
Wojtek <wskozlowski@dbzero.io>
2025-12-04 10:04:49 +0100

    license

--- dbzero/setup.py
+++ dbzero/setup.py
@@ -11,4 +11,10 @@ setup(
     description='DBZero community edition',
     packages=['dbzero'],
     python_requires='>=3.8',
+    license='AGPL-3.0-or-later',
+    classifiers=[
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
 )
