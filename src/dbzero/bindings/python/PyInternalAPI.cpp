dd02e9f0aaca21d2b55061c48c73ccf9b35fbec8
Adrian Zawadzki <azawadzki@dbzero.io>
2025-12-09 12:24:08 +0100

    fix(build): fixed build on debug

--- src/dbzero/bindings/python/PyInternalAPI.cpp
+++ src/dbzero/bindings/python/PyInternalAPI.cpp
@@ -1039,15 +1039,13 @@ namespace db0::python
             auto result = Py_OWN(tryCopyPrefixImpl(*storage, output_file_name, page_io_step_size, meta_io_step_size));
             storage->close();
             return result.steal();
-            if (!result) {
-                return nullptr;
-            }
         } catch (...) {
             if (storage) {
                 storage->close();
             }
             throw;
         }
+        return nullptr;
     }
     
     template PyObject *getMaterializedMemoObject(MemoObject *);
