eba9a57a62e8a62738ea9ba90599421e1921acbe
Wojciech Kozłowski <96179710+wskozlowski@users.noreply.github.com>
2026-04-20 16:19:22 +0200

    bugfix: client-side lang cache refresh fixed (to prevent pulling stal… (#743)
    * bugfix: client-side lang cache refresh fixed (to prevent pulling stale objects)
    
    * version update
    
    ---------
    
    Co-authored-by: Wojtek <wskozlowski@dbzero.io>

--- dbzero/setup.py
+++ dbzero/setup.py
@@ -10,7 +10,7 @@ if sys.byteorder == 'big':
 
 setup(
     name='dbzero',
-    version='0.2.1',
+    version='0.2.2',
     description='DBZero community edition',
     packages=['dbzero'],
     python_requires='>=3.9',
