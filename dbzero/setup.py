ed08a8d3c02720a7f3c3c5400c557687fae04d31
Adrian Zawadzki <azawadzki@dbzero.io>
2026-05-20 21:10:14 +0200

    release 0.3.0

--- dbzero/setup.py
+++ dbzero/setup.py
@@ -10,7 +10,7 @@ if sys.byteorder == 'big':
 
 setup(
     name='dbzero',
-    version='0.2.4',
+    version='0.3.0',
     description='DBZero community edition',
     packages=['dbzero'],
     python_requires='>=3.9',
