17e6ad03c3fe2a26635531648add503d0fe39393
Wojtek <wskozlowski@dbzero.io>
2026-06-15 16:37:41 +0200

    version update

--- dbzero/setup.py
+++ dbzero/setup.py
@@ -10,7 +10,7 @@ if sys.byteorder == 'big':
 
 setup(
     name='dbzero',
-    version='0.3.5',
+    version='0.3.6',
     description='DBZero community edition',
     packages=['dbzero'],
     python_requires='>=3.9',
